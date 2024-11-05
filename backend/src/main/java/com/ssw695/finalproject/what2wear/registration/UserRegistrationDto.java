c452720b5a5e0ff8cedc4a6af2ed503a33a0a765
Harsha Kuppala <157903871+harshav-04@users.noreply.github.com>
2024-11-04 19:15:21 -0500

    Merging Karthik's backend code to main

---
+++ backend/src/main/java/com/ssw695/finalproject/what2wear/registration/UserRegistrationDto.java
@@ -0,0 +1,13 @@
+package com.ssw695.finalproject.what2wear.registration;
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+	private String username;
+    private String password;
+
+   
+}
---
+++ backend/src/test/java/com/ssw695/finalproject/what2wear/What2wearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssw695.finalproject.what2wear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What2wearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
